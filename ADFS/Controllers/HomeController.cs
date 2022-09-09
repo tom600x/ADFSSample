9cacd5a0de00da5e79d7c62e029864755c8e7451
Tom Ordille <tordille@gmail.com>
2022-09-09 13:08:56 -0400

    Add project files.

---
+++ ADFS/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Security.Claims;
+
+namespace ADFS.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            // Microsoft.IdentityModel.Tokens.
+
+            return View();
+        }
+
+        public ActionResult ShowClaims()
+        {
+            ViewBag.Message = "Your application description page.";
+            return View((User as ClaimsPrincipal).Claims);
+
+            
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
